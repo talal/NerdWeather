3be27aeaf467c6cae526a979ad8428eef5c1fc66
Muhammad Talal Anwar <3526562+talal@users.noreply.github.com>
2018-01-13 03:44:20 +0100

    add Weather API
    Adding OpenWeatherMap API and the corresponding changes.

--- app/src/main/java/com/emiappnw/nerdweather/MainActivity.java
+++ app/src/main/java/com/emiappnw/nerdweather/MainActivity.java
@@ -2,7 +2,6 @@ package com.emiappnw.nerdweather;
 
 import android.content.Context;
 import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
@@ -13,51 +12,84 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView currentLocationField, weatherDescriptionField, currentTempField, minTempCField, maxTempCField, WindField, HumidityField, PressureField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //              Begin API Stuff              //
+        currentLocationField = findViewById(R.id.currentLoc);
+        weatherDescriptionField = findViewById(R.id.weatherDescription);
+        currentTempField = findViewById(R.id.currentTemp);
+        // minTempCField = findViewById(R.id.minTempC); // We don't need minimum temp at the moment
+        // maxTempCField = findViewById(R.id.maxTempC); // We don't need maximum temp at the moment
+        WindField = findViewById(R.id.Wind);
+        HumidityField = findViewById(R.id.Humidity);
+        PressureField = findViewById(R.id.Pressure);
+
+        WeatherAPI.placeIdTask asyncTask = new WeatherAPI.placeIdTask(new WeatherAPI.AsyncResponse() {
+            public void processFinish(String weather_location, String weather_description, String weather_temperature, String weather_temperature_min, String weather_temperature_max, String weather_wind, String weather_humidity, String weather_pressure) {
+
+            currentLocationField.setText(weather_location);
+            weatherDescriptionField.setText(weather_description);
+            currentTempField.setText(weather_temperature);
+            // minTempCField.setText(weather_temperature_min); // We don't need minimum temp at the moment
+            // maxTempCField.setText(weather_temperature_max); // We don't need maximum temp at the moment
+            WindField.setText("W: "+weather_wind);
+            HumidityField.setText("H: "+weather_humidity);
+            PressureField.setText("P: "+weather_pressure);
+
+            }
+        });
+
+
+        asyncTask.execute("51.05", "13.74"); // ("Latitude", "Longitude")
+        //              End API Stuff              //
     }
 
     public void onButtonShowPopupWindowClick(View view) {
 
-        // reference main layout
+        // Reference main layout
         ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
 
-        //Set popup window size
+        // Set popup window size
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         int width = size.x - 100;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
 
-        // inflate popup window layout
+        // Inflate popup window layout
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
 
         View popupView = inflater.inflate(R.layout.dialog_temp_stats, null);
 
 
 
-        // create the popup window
-        boolean focusable = true; // lets taps outside the popup also dismiss it
+        // Create the popup window
+        boolean focusable = true; // Lets taps outside the popup also dismiss it
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, true);
 
-        //animation
+        // Animation
         popupWindow.setBackgroundDrawable(new ColorDrawable( android.graphics.Color.TRANSPARENT));
 
         popupWindow.setAnimationStyle(R.style.Animation);
 
-        //show popup
+        // Show popup
         popupWindow.showAtLocation(mainLayout, Gravity.CENTER_HORIZONTAL, 0, -300);
+
     }
 
-    //Inject Calligraphy into Context
+    // Inject Calligraphy into Context
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
---
+++ app/src/main/java/com/emiappnw/nerdweather/WeatherAPI.java
@@ -0,0 +1,101 @@
+package com.emiappnw.nerdweather;
+
+import android.os.AsyncTask; // AsyncTask enables proper and easy use of the UI thread.
+import android.util.Log; // API for sending log output.
+
+import org.json.JSONObject; // Self-explanatory.
+import org.json.JSONException; // Thrown to indicate a problem with the JSON API.
+
+import java.io.BufferedReader; // Read text from an input-stream and buffer the characters for efficient reading.
+import java.io.InputStreamReader; // Reads bytes and decodes them into characters using a specified charset.
+import java.net.HttpURLConnection; // A URLConnection with support for HTTP-specific features.
+import java.net.URL; // Self-explanatory.
+import java.util.Locale; // Self-explanatory.
+
+public class WeatherAPI {
+    private static final String API_URL = "http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
+    private static final String API_KEY = "d87d09e51607cadfe08107106806f23a";
+
+
+    // Get Weather Data from OpenWeatherMap API
+    public static JSONObject getWeatherJSON(String lat, String lon){
+        try {
+            URL url = new URL(String.format(API_URL, lat, lon));
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.addRequestProperty("x-api-key", API_KEY);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+            JSONObject data = new JSONObject(json.toString());
+
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+            return data;
+        }
+        catch(Exception e) {
+            return null;
+        }
+    }
+
+
+    public interface AsyncResponse {
+        void processFinish(String output1, String output2, String output3, String output4, String output5, String output6, String output7, String output8);
+    }
+
+
+    public static class placeIdTask extends AsyncTask<String, Void, JSONObject> {
+
+        public AsyncResponse delegate = null;
+
+        public placeIdTask(AsyncResponse asyncResponse) {
+            delegate = asyncResponse;
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+            JSONObject jsonWeather = null;
+            try {
+                jsonWeather = getWeatherJSON(params[0], params[1]);
+            } catch (Exception e) {
+                Log.d("Error", "Cannot process JSON results", e);
+            }
+
+
+            return jsonWeather;
+        }
+
+        // Get the weather data from JSON and and assign it to our variables
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if(json != null){
+                    JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+                    JSONObject main = json.getJSONObject("main");
+
+
+                    String location = json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country");
+                    String description = details.getString("description").toUpperCase(Locale.US);
+                    String temperature = String.format("%.0f", main.getDouble("temp"))+ "°";
+                    String temperature_min = String.format("%.0f", main.getDouble("temp_min"))+ "°";
+                    String temperature_max = String.format("%.0f", main.getDouble("temp_max"))+ "°";
+                    String wind = String.format("%.1f", json.getJSONObject("wind").getDouble("speed"))+ " m/s";
+                    String humidity = main.getString("humidity") + "%";
+                    String pressure = main.getString("pressure") + " hPa";
+
+                    delegate.processFinish(location, description, temperature, temperature_min, temperature_max, wind, humidity, pressure);
+
+                }
+            } catch (JSONException e) {
+                //Log.e(LOG_TAG, "Cannot process JSON results", e);
+            }
+        }
+    }
+}
